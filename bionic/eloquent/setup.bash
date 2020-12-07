4f450d4c268e6fb20da3c002903a962930091f10
RyodoTanaka <groadpg@gmail.com>
2020-12-07 16:06:16 +0900

    Add eloquent setup

---
+++ bionic/eloquent/setup.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Install ros eloquent desktop #
+sudo apt-get update
+sudo apt-get install -y curl gnupg2 lsb-release
+curl -Ls https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
+sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+sudo apt-get update
+sudo apt-get install -y ros-eloquent-desktop
+
+# Enable ROS #
+echo "source /opt/ros/eloquent/setup.bash" >> /home/ubuntu/.bashrc
+source /home/ubuntu/.bashrc
+
+# Install rosdep vcstools #
+sudo apt-get install -yq python3-rosdep
+sudo apt-get install -yq python3-rosinstall
+sudo apt-get install -yq python3-rosinstall-generator
+sudo apt-get install -yq python3-wstool
+sudo apt-get install -yq python3-vcstool
+sudo apt-get install -yq python3-vcstools
+sudo apt-get install -yq python3-colcon-common-extensions
+sudo apt-get install -yq python3-argcomplete
+# Init rosdep #
+sudo rosdep init
+rosdep update
